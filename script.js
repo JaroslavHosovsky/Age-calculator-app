17eecb79b4813df79910fb512545a4daf4c30850
Jaroslav Hošovský <j.hosovsky@seznam.cz>
2024-03-01 12:48:49 +0100

    Conditions handling valid date and empty fields added

--- script.js
+++ script.js
@@ -12,6 +12,78 @@ const getBirthDate = () => {
 };
 
 function calculateAge() {
+  // Field required error messages
+  const required1 = document.getElementById("required1");
+  const required2 = document.getElementById("required2");
+  const required3 = document.getElementById("required3");
+
+  if (dayInput.value === "") {
+    required1.style.display = "block";
+  } else {
+    required1.style.display = "none";
+  }
+
+  if (monthInput.value === "") {
+    required2.style.display = "block";
+  } else {
+    required2.style.display = "none";
+  }
+
+  if (yearInput.value === "") {
+    required3.style.display = "block";
+  } else {
+    required3.style.display = "none";
+  }
+
+  // Invalid value error message
+  const dayInvalid = document.querySelector(".error-messages1 p:nth-child(2)");
+  const monthInvalid = document.querySelector(
+    ".error-messages2 p:nth-child(2)"
+  );
+  const yearInvalid = document.querySelector(".error-messages3 p:nth-child(2)");
+
+  if (dayInput.value === "") {
+    dayInvalid.style.display = "none";
+  } else if (dayInput.value <= 0) {
+    dayInvalid.style.display = "block";
+  } else {
+    dayInvalid.style.display = "none";
+  }
+
+  if (monthInput.value === "") {
+    monthInvalid.style.display = "none";
+  } else if (monthInput.value > 12 || monthInput.value <= 0) {
+    monthInvalid.style.display = "block";
+  } else if (["2"].includes(monthInput.value) && dayInput.value > 28) {
+    dayInvalid.style.display = "block";
+    monthInvalid.style.display = "none";
+  } else if (
+    ["4", "6", "9", "11"].includes(monthInput.value) &&
+    dayInput.value > 30
+  ) {
+    dayInvalid.style.display = "block";
+    monthInvalid.style.display = "none";
+  } else if (
+    ["1", "3", "5", "7", "8", "10", "12"].includes(monthInput.value) &&
+    dayInput.value > 31
+  ) {
+    dayInvalid.style.display = "block";
+    monthInvalid.style.display = "none";
+  } else {
+    monthInvalid.style.display = "none";
+  }
+
+  if (yearInput.value === "") {
+    yearInvalid.style.display = "none";
+  } else if (yearInput.value > 2024) {
+    yearInvalid.style.display = "block";
+  } else if (yearInput.value <= 1900) {
+    yearInvalid.style.display = "block";
+    yearInvalid.textContent = "I hope you're not that old";
+  } else {
+    yearInvalid.style.display = "none";
+  }
+
   const currentDate = new Date();
   const birthDate = new Date(getBirthDate());
 
